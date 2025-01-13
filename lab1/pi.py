bf5cee9c4c127865bcf3ea46eced103fe96d58ca
Hernan Barquero <elmer.barquero@ucr.ac.cr>
2025-01-13 13:24:06 -0600

    terminando mi solucion de gabo

--- lab1/pi.py
+++ lab1/pi.py
@@ -2,22 +2,29 @@
 
 import numpy as np
 
+# Esta es una función para integrar con método de Suma de Riemann,
+# para usarla se requiere definir la función del integrando.
+
+# Función integrando
+# x corresponde a la variable independiente de la función
 def integrando(x):
-    f = 1 /(1+x**2)
+    f = 4 /(1+x**2)
     return f
 
+# Función Suma de Riemann
+# Integrando -> Función a integrar
+# x_inf  -> Limite inferior de la integral
+# x_sup -> Limite superior de la integral
+# NumeroCuadros -> Numero de rectangulos  usandos en la aproximación
 def SumaRiemann(integrando,x_inf,x_sup,NumeroCuadros):
-    h = (x_sup - x_inf)/NumeroCuadros
-    
-    centro_rectangulo = h/2
+    h = (x_sup-x_inf)/NumeroCuadros #Distancia entre pasos
 
-    x = np.linspace(x_inf + centro_rectangulo,x_sup,NumeroCuadros)
-
-    aux = integrando(x)*h
-    
-    I =4*np.sum(aux)
-    
+    I = 0
+    for i in range(NumeroCuadros):
+        I += integrando(x_inf+(i+0.5)*h)*h 
     return I
+    
 
 print(SumaRiemann(integrando, 0,1,100))
+
 
