e01c10c3a187dea4c18305a338e30fc7bfae5e34
Hernan Barquero <elmer.barquero@ucr.ac.cr>
2025-01-21 23:50:26 -0600

    corriendo de nuevo filtro grep tarea 1

--- tarea1/generar_files_i_N.sh
+++ tarea1/generar_files_i_N.sh
@@ -4,7 +4,7 @@ for i in $(seq 0 ${1}); do
 	touch rand_${i}_${2}.dat
 	for j in $(seq 1 ${2}); do
 		out=$(./random.sh)
-		first_number=$(echo "$out" | grep -E '^[1-9]+$' | head -n 1)
+		first_number=$(echo "$out" | grep -E '^[0-9]+$' | head -n 1)
 		echo "$first_number" >> rand_${i}_${2}.dat
 	done
 done
